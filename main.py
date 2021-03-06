ebdfb059126dbf59392abbea44c2d67446171bea
CobbCoding1 <rircobb@gmail.com>
2021-03-06 22:41:11 +0000

    Added meme template picker (needs to become a regular meme, will deal with later)

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import discord
+import requests
+import json
+import random
 import os
 
 client = discord.Client()
+
+def get_meme():
+  response = requests.get('https://api.imgflip.com/get_memes')
+  json_data = json.loads(response.text)
+  return(json_data['data']['memes'][random.randint(0, 10)]['url'])
 
 @client.event
 async def on_ready():
@@ -14,5 +22,7 @@ async def on_message(message):
   
   if(message.content.startswith('.currency')):
     await message.channel.send('You have earned +1 currency!')
+  if(message.content.startswith('.currency') and message.content.endswith('meme')):
+    await message.channel.send(get_meme())
 
 client.run(os.getenv('TOKEN'))
