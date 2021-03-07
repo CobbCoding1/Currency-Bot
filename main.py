ae38c5af80b417abb1e7756dbc8c098afcc8f9b5
CobbCoding1 <rircobb@gmail.com>
2021-03-07 00:16:33 +0000

    Fixed some things with user verification.

--- main.py
+++ main.py
@@ -13,14 +13,23 @@ def get_meme():
   return(json_data['data']['memes'][random.randint(0, 10)]['url'])
 
 def add_currency(message):
-  print(message.author.id)
-  print(db[str(message.author.id)])
-  db[str(message.author.id)] = str(int(db[str(message.author.id)]) + 10)
+  print(str(message.author.name))
 
-  print(db[str(message.author.id)])
+  #print(message.author.id)
+  if(str(message.author.name) in db.keys()):
+    value = db[str(message.author.name)]
+    print("lel" + db[message.author.name])
+    db[str(message.author.name)] = str(int(value) + 10)
+    print(12345)
+  else:
+    db[str(message.author.name)] = str(message.author.name)
+    db[message.author.name] = str(10)
+    print(54321)
+
+  print(db[str(message.author.name)])
 
 def get_currency(message):
-  return("You currently have: " + db[str(message.author.id)])
+  return("You currently have: " + db[str(message.author.name)])
   
 
 @client.event
@@ -35,7 +44,7 @@ async def on_message(message):
   msg = message.content
   
   add_currency(message)
-  await message.channel.send('You have earned +10 currency!')
+  await message.channel.send('You have earned +10 currency! -THIS IS A DEBUG MESSAGE, WILL REMOVE LATER')
 
   if(msg.startswith('.currency') and msg.endswith('meme')):
     await message.channel.send(get_meme())
